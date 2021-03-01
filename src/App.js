08bc92f7ff3a07b5755841308731c02032570a60
Manthan Kumbhar <kum.manthan@gmail.com>
2021-03-01 22:59:40 +0530

    PR approved for practice branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Input from "./Input";
 
 function App() {
   return (
     <div className="App">
       <h1>Github, Discord, API calls & me v2.0</h1>
+      <Input />
     </div>
   );
 }
---
+++ src/Input.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+import React, { Component } from "react";
+
+class Input extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputVal: "",
+      persons: [],
+      github_url: "",
+    };
+    this.updateInputValue = this.updateInputValue.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  updateInputValue(event) {
+    event.preventDefault();
+    this.setState({ inputVal: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.inputVal === "") {
+      alert("Please enter a url");
+    } else {
+      axios
+        .post(
+          "https://v2-github-discord-api-and-me.herokuapp.com/github_discord_urls",
+          {
+            url: this.state.inputVal,
+          }
+        )
+        .then((res) => {
+          this.setState({ github_url: res.data });
+          console.log(res.data);
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    }
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="url"
+          value={this.state.inputValue}
+          placeholder="Please enter your discord webhook link.."
+          onChange={(event) => this.updateInputValue(event)}
+          className="input"
+        />
+        <input
+          type="submit"
+          value="Submit"
+          className="btn btn-success"
+          style={{ margin: "10px" }}
+        />
+        <br />
+        <br />
+        <p>The link to be pasted in github will appear below:</p>
+        <p>{this.state.github_url}</p>
+      </form>
+    );
+  }
+}
+
+export default Input;
